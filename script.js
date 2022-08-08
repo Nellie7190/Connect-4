b36fa4afec191a9fe331b58fa39b4d21a79fbd48
Your Name <nelliebosch@Nellies-MacPro.lan>
2022-08-08 18:35:20 -0400

    made basic variables, and board w/o tile slots

---
+++ script.js
@@ -0,0 +1,12 @@
+//Red and Yellow Players and game start with Red
+const redPlay = "R";
+const yelPlay = "Y";
+let curPlay = redPlay;
+
+//endGame changes after win
+let endGame = false;
+let board;
+
+//Board setup
+const rows = 6;
+const columns = 7;
