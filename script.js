bf7c62348cee883bac38fc88da876a3d94cdc3ce
Your Name <nelliebosch@Nellies-MacPro.lan>
2022-08-08 18:36:32 -0400

    created board w/ js now has tile slots, no functionality

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ let board;
 //Board setup
 const rows = 6;
 const columns = 7;
+
+// Game setup on load
+window.onload = function() {
+    setGame();
+}
+
+//populate tiles on board
+const setGame = () => {
+    board = [];
+    
+    //Goes through the entire board by row, then col
+    for (let rowId = 0; rowId < rows; rowId++) {
+        let row = [];
+        for (let colId = 0; colId < columns; colId ++) {
+            //Creating Empty holes for peices on board
+            row.push(' ');
+
+            //HTML
+            //creating <div> id='0-0' class='tile' <div>
+            let tile = document.createElement('div');
+            //Giving each tile a unique ID based on placement
+            tile.id = rowId.toString()+ "-" + colId.toString();
+            //Adding a class of 'tile' to each tile
+            tile.classList.add('tile');
+            document.getElementById('board').append(tile)
+        }
+        
+        board.push(row)
+    }
+}
